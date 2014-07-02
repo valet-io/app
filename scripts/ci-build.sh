2804089fee8fa54d72de746d8b63a4393e5c59a6
Ben Drucker <bvdrucker@gmail.com>
2014-07-02 13:11:29 -0400

    refactor CI build

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,9 +1,7 @@
 if [ "$TRAVIS_TAG" ]; then
   ./node_modules/.bin/gulp build --production
+elif [ "$TRAVIS_BRANCH" == "master" ]; then
+  ./node_modules/.bin/gulp build --staging
 else
-  if [ "$TRAVIS_BRANCH" == "master" ]; then
-    ./node_modules/.bin/gulp build --staging
-  else
-    ./node_modules/.bin/gulp build --development
-  fi
+  ./node_modules/.bin/gulp build --development
 fi
