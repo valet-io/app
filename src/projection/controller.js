fbe201120c9de0196715aa808f6dec54d922ead8
Ben Drucker <bvdrucker@gmail.com>
2014-09-08 18:21:45 -0400

    Handle browsers that do not support full screen
    [Finishes #78421058]

--- src/projection/controller.js
+++ src/projection/controller.js
@@ -7,7 +7,9 @@ var screenfull = require('screenfull');
 module.exports = function ($scope, campaign, $document) {
   $scope.campaign = campaign;
   $scope.fullscreen = screenfull;
-  $document.on(screenfull.raw.fullscreenchange, function () {
-    $scope.$digest();
-  });
+  if (screenfull) {
+    $document.on(screenfull.raw.fullscreenchange, function () {
+      $scope.$digest();
+    });
+  }
 };
