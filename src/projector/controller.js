d85f4ad904a16689a366716d0cbca1f88dec0baf
Ben Drucker <bvdrucker@gmail.com>
2014-11-14 14:57:48 -0500

    chore(projector): clean up projector controller code

--- src/projector/controller.js
+++ src/projector/controller.js
@@ -1,16 +1,6 @@
 'use strict';
 
-// TODO: Clean up and move full screen logic to a proper service
-
-var screenfull = require('screenfull');
-
-module.exports = function ($scope, campaign, $document) {
-  // campaign.pledges.$subscribe();
-  $scope.fullscreen = screenfull;
-  if (screenfull) {
-    $document.on(screenfull.raw.fullscreenchange, function () {
-      $scope.$digest();
-    });
-  }
+module.exports = function ($scope) {
+  $scope.fullscreen = require('screenfull');;
 };
-module.exports.$inject = ['$scope', 'campaign', '$document'];
+module.exports.$inject = ['$scope'];
