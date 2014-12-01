fb434f79e1059a3524dc40858c1a81601f809805
Ben Drucker <bvdrucker@gmail.com>
2014-12-01 09:51:29 -0500

    fix(project): add assets domain to whitelist so templates can be loaded

--- src/projector/index.js
+++ src/projector/index.js
@@ -9,6 +9,15 @@ require('angular')
   .value('screenfull', require('screenfull'))
   .controller('ProjectorController', require('./controller'))
   .filter('reverse', require('./reverse'))
-  .config(require('./states'));
+  .config(require('./states'))
+  .config(allowAssets);
+
+function allowAssets ($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    'http://assets.valet.io/**'
+  ]);
+}
+allowAssets.$inject = ['$sceDelegateProvider'];
 
 module.exports = 'Projector';
