c8d91d8110374d248cb34f253c3b5ed7f0186d96
Ben Drucker <bvdrucker@gmail.com>
2014-12-07 10:53:16 -0500

    feat(campaign): add a basic dashboard listing pledges

--- src/campaign/states.js
+++ src/campaign/states.js
@@ -17,6 +17,13 @@ module.exports = function ($stateProvider) {
       resolve: {
         campaign: campaign
       }
+    })
+    .state('campaign.dashboard', {
+      templateUrl: '/views/campaign/dashboard.html',
+      url: '/dashboard',
+      resolve: {
+        campaign: subscribe
+      }
     });
 };
 module.exports.$inject = ['$stateProvider'];
@@ -25,3 +32,14 @@ function campaign (Campaign, $stateParams) {
   return new Campaign({id: $stateParams.id}).$fetch();
 }
 campaign.$inject = ['Campaign', '$stateParams'];
+
+function subscribe (campaign, $q) {
+  return $q.all([
+    campaign.$subscribe(['aggregates', 'options'], true),
+    campaign.pledges.$subscribe()
+  ])
+  .then(function () {
+    return campaign;
+  });
+}
+subscribe.$inject = ['campaign', '$q'];
