d42d373def653336eec6aa7b0b9ccad998a9c566
Ben Drucker <bvdrucker@gmail.com>
2015-02-27 18:10:16 -0500

    Import angular-loading module

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 import angular from 'angular';
+import loading from 'angular-loading'
 import AppController from './controller';
 
-export default angular.module('valetApp.app', [])
-  .controller('AppController', AppController)
-  .name;
+export default angular.module('valetApp.app', [
+  loading
+])
+.controller('AppController', AppController)
+.name;
