dbbcb1024a3ba9c661f533aaf38e1b5bcd112576
Ben Drucker <bvdrucker@gmail.com>
2015-03-10 10:32:50 -0400

    Hook up pledge filtering

--- src/campaign/controller.js
+++ src/campaign/controller.js
@@ -3,6 +3,10 @@
 CampaignController.$inject = ['$scope', 'campaign'];
 function CampaignController ($scope, campaign) {
   $scope.campaign = campaign;
+  $scope.search = {};
+  $scope.filterPledges = function (pledge) {
+    return !$scope.search.name || (pledge.donor.name.indexOf($scope.search.name) !== -1);
+  };
 }
 
 export default CampaignController;
