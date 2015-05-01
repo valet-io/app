b5e5dc5bd579e39add3ac6bb2ce425010e8f423d
Ben Drucker <bvdrucker@gmail.com>
2015-05-01 14:46:11 -0400

    Replace row-extra slidedown with simple click

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -2,10 +2,7 @@
 
 import angular from 'angular';
 import statistic from './statistic';
-import {row, extra} from './row';
 
 export default angular.module('valetApp.dashboard', [])
   .directive('statistic', statistic)
-  .directive('pledgeRow', row)
-  .directive('pledgeRowExtra', extra)
   .name;
